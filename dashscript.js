3cc9dcbd47a70cbf7c14cb8c401a46cb422829dc
Kruti Dugade <krutidugade@gmail.com>
2026-06-18 16:42:44 +0100

    Goggle SSO login and dashboard to create and list events.

---
+++ dashscript.js
@@ -0,0 +1,60 @@
+const token = localStorage.getItem("token");
+
+// if not logged in → go back
+if (!token) {
+  window.location.href = "index.html";
+}
+
+// decode token (just for display)
+const payload = JSON.parse(atob(token.split('.')[1]));
+
+document.getElementById("user").innerHTML =
+  "Hello " + payload.name + "<br>" + payload.email;
+
+function logout() {
+  localStorage.removeItem("token");
+  window.location.href = "index.html";
+}
+
+let events = JSON.parse(localStorage.getItem("events")) || [];
+
+function saveDB() {
+  localStorage.setItem("events", JSON.stringify(events));
+}
+
+function render() {
+  const list = document.getElementById("list");
+  list.innerHTML = "";
+
+  events.forEach((event, i) => {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+      ${event}
+      <button onclick="deleteEvent(${i})">X</button>
+    `;
+
+    list.appendChild(li);
+  });
+}
+
+function createEvent() {
+  const name = document.getElementById("name").value;
+  if (!name) return;
+
+  events.push(name);
+
+  saveDB();
+  render();
+
+  document.getElementById("name").value = "";
+}
+
+function deleteEvent(index) {
+  events.splice(index, 1);
+
+  saveDB();
+  render();
+}
+
+render();

efc44c9c0df764cddcfad85cd78cb0563f904855
Kruti Dugade <33723519+krutidugade@users.noreply.github.com>
2026-07-13 16:39:20 +0100

    Delete eventscript.js
    Removed duplicate file after moving into folder.

--- eventscript.js
+++
@@ -1,9 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-
-const id = params.get("id");
-
-const events = JSON.parse(localStorage.getItem("events")) || [];
-
-const eventName = events[id];
-
-document.getElementById("name").innerText = eventName;
